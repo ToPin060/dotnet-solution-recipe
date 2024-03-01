94bd755d54f59fabe7711da7fe0ae118c6938b2e
Tristan PINTO <tristan.pin81@gmail.com>
2024-03-01 14:19:19 +0100

    Implement XlmRecipesService

---
+++ Services/XmlRecipesService.cs
@@ -0,0 +1,27 @@
+﻿using DC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Services
+{
+    public class XmlRecipesService : RecipesService
+    {
+        public override List<Recipe> GetAll()
+        {
+            XDocument xdoc = XDocument.Load("datas\\recipes.xml");
+            var recipes = xdoc.Descendants("recipe")
+                .Select(recipeElement => new Recipe
+                {
+                    Id = Guid.Parse(recipeElement.Attribute("id")?.Value),
+                    Title = recipeElement.Attribute("title")?.Value
+                })
+                .ToList();
+
+            return recipes;
+        }
+    }
+}
